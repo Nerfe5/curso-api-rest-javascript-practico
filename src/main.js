39fde0ca7249b9f7a9695c1f329ce90051f7f7cd
Nerfe5 <nerfe5@icloud.com>
2022-07-11 16:02:08 -0500

    Correciones al HTML y primeras pruebas de fetch trending

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log ('Hello world');
+async function getTrendingMoviesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+
+    const data = await res.json();
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const moviePoster = document.createElement('img');
+        moviePoster.classList.add('movie-poster');
+        moviePoster.setAttribute('alt', movie.title);
+        moviePoster.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + movie.poster_path);
+
+    }
+    );
+}
+
+getTrendingMoviesPreview();
