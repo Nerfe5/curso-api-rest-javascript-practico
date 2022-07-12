f6c062dfe172e63d6321f967119cc12367be447f
Nerfe5 <nerfe5@icloud.com>
2022-07-12 16:02:05 -0500

    Conclusión de lista de tendencias, categorias y migración a axios

--- src/main.js
+++ src/main.js
@@ -1,20 +1,67 @@
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+   headers:{
+         'Content-Type': 'application/json;charset=utf-8',
+   },
+    params: {   
+        'api_key': API_KEY,
+    }
+});
+
+
+
 async function getTrendingMoviesPreview(){
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const { data } = await api('trending/movie/day');
+     const movies = data.results;
 
-    const data = await res.json();
-
-    const movies = data.results;
+    console.log({data, movies});
     movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector
+        ('#trendingPreview .trendingPreview-movieList');
+
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
         const moviePoster = document.createElement('img');
-        moviePoster.classList.add('movie-poster');
+        moviePoster.classList.add('movie-img');
         moviePoster.setAttribute('alt', movie.title);
-        moviePoster.setAttribute('src', 'https://image.tmdb.org/t/p/w500/' + movie.poster_path);
+        moviePoster.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path,);
+
+
+        movieContainer.appendChild(moviePoster);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    }
+    );
+}
+
+async function getCategoriesPreview(){
+    const { data }= await api('genre/movie/list');
+    const categories = data.genres;
+
+    console.log({data, categories});
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector
+        ('#categoriesPreview .categoriesPreview-list')
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+        
+        
+        
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode('category.name');
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
 
     }
     );
 }
 
 getTrendingMoviesPreview();
+getCategoriesPreview();
+
